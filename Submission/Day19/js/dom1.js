b67fbb4143f72d7a7069b9b7a35f55187eb165da
monocosmo <shuchiwu@gmail.com>
2018-06-18 12:47:44 -0400

    Add files via upload

--- Submission/Day19/js/dom1.js
+++ Submission/Day19/js/dom1.js
@@ -2,7 +2,7 @@
 function getAllListItem() {
     // 返回页面中所有li标签
     var elements = document.getElementsByTagName("li");
-    output = document.getElementById("q1");
+    var output = document.getElementById("q1");
     for(var i = 0; i < elements.length; i++){
     	var para = document.createElement("p");
     	para.textContent = elements[i].firstChild.innerHTML + "(span): " + elements[i].lastChild.innerHTML;
@@ -14,7 +14,7 @@ function findAllHtmlSpanInOneSection(sectionId) {
     // 返回某个section下所有span中内容为HTML的span标签
     var div1 = document.getElementById(sectionId);
     var elements = div1.getElementsByTagName("span");
-    output = document.getElementById("q2");
+    var output = document.getElementById("q2");
     for(var i = 0; i < elements.length; i++){
     	if (elements[i].innerHTML == "HTML"){
     		var para = document.createElement("p");
@@ -28,7 +28,7 @@ function findListItem(sectionId, spanCont) {
     // 返回某个section下，所有所包含span内容为spanCont的LI标签
     var div1 = document.getElementById(sectionId);
     var elements = div1.getElementsByTagName("span");
-    output = document.getElementById("q3");
+    var output = document.getElementById("q3");
     for(var i = 0; i < elements.length; i++){
         if (elements[i].innerHTML == spanCont){
             var para = document.createElement("p");
@@ -42,7 +42,7 @@ function getActiveLinkContent(sectionId) {
     // 返回某个section下，class为active的链接中包含的文字内容
     var div1 = document.getElementById(sectionId);
     var elements = div1.getElementsByClassName("active");
-    output = document.getElementById("q4");
+    var output = document.getElementById("q4");
     for(var i = 0; i < elements.length; i++){
     	var para = document.createElement("p");
     	para.textContent = elements[i].innerHTML;
--- Submission/Day19/js/dom2.js
+++ Submission/Day19/js/dom2.js
@@ -1,16 +1,51 @@
 // Solution with querySelector and querySelectorAll
 function getAllListItem() {
     // 返回页面中所有li标签
+    var elements = document.querySelectorAll("li");
+    var output = document.getElementById("q1");
+    for(var i = 0; i < elements.length; i++){
+    	var para = document.createElement("p");
+    	para.textContent = elements[i].firstChild.innerHTML + "(span): " + elements[i].lastChild.innerHTML;
+    	output.appendChild(para);
+	}
 }
 
 function findAllHtmlSpanInOneSection(sectionId) {
     // 返回某个section下所有span中内容为HTML的span标签
+    var div1 = document.querySelector("div#"+sectionId);
+    var elements = div1.querySelectorAll("span");
+    var output = document.getElementById("q2");
+    for(var i = 0; i < elements.length; i++){
+    	if (elements[i].innerHTML == "HTML"){
+    		var para = document.createElement("p");
+    		para.textContent = elements[i].parentNode.children[1].innerHTML;
+    		output.appendChild(para);
+    	}
+    }
 }
 
 function findListItem(sectionId, spanCont) {
     // 返回某个section下，所有所包含span内容为spanCont的LI标签
+    var div1 = document.querySelector("div#"+sectionId);
+    var elements = div1.querySelectorAll("span");
+    var output = document.getElementById("q3");
+    for(var i = 0; i < elements.length; i++){
+        if (elements[i].innerHTML == spanCont){
+            var para = document.createElement("p");
+            para.textContent = elements[i].parentNode.children[1].innerHTML;
+            output.appendChild(para);
+        }
+    }
 }
 
 function getActiveLinkContent(sectionId) {
     // 返回某个section下，class为active的链接中包含的文字内容
+    var div1 = document.querySelector("div#"+sectionId);
+    var elements = div1.querySelectorAll("a.active");
+    var output = document.getElementById("q4");
+    for(var i = 0; i < elements.length; i++){
+    	var para = document.createElement("p");
+    	para.textContent = elements[i].innerHTML;
+    	output.appendChild(para);
+    }
 }
