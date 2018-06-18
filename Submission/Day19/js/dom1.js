830b9b4940b5fd64dc87bc4b9841dc22dae618af
monocosmo <shuchiwu@gmail.com>
2018-06-17 23:55:19 -0400

    Add files via upload

---
+++ Submission/Day19/js/dom1.js
@@ -0,0 +1,44 @@
+// Solution with getElementById(), getElementsByTagName(), childNodes, parentNode
+function getAllListItem() {
+    // 返回页面中所有li标签
+    var elements = document.getElementsByTagName("li");
+    output = document.getElementById("q1");
+    for(var i = 0; i < elements.length; i++){
+    	var para = document.createElement("p");
+    	para.textContent = (i+1) + ": " + elements[i].firstChild.innerHTML + " " + elements[i].lastChild.innerHTML;
+    	output.appendChild(para);
+    }
+}
+
+function findAllHtmlSpanInOneSection(sectionId) {
+    // 返回某个section下所有span中内容为HTML的span标签
+    var div1 = document.getElementById(sectionId);
+    var elements = div1.getElementsByTagName("span");
+    output = document.getElementById("q2");
+    for(var i = 0; i < elements.length; i++){
+    	if (elements[i].innerHTML == "HTML"){
+    		var para = document.createElement("p");
+    		para.textContent = (i+1) + ": " + elements[i].parentNode.children[1].innerHTML;
+    		output.appendChild(para);
+    	}
+    }
+}
+
+function findListItem(sectionId, spanCont) {
+    // 返回某个section下，所有所包含span内容为spanCont的LI标签
+    // I don't understand what is "spanCont"
+}
+
+function getActiveLinkContent(sectionId) {
+    // 返回某个section下，class为active的链接中包含的文字内容
+    var div1 = document.getElementById(sectionId);
+    var elements = div1.getElementsByTagName("span");
+    output = document.getElementById("q2");
+    for(var i = 0; i < elements.length; i++){
+    	if (elements[i].innerHTML == "HTML"){
+    		var para = document.createElement("p");
+    		para.textContent = (i+1) + ": " + elements[i].parentNode.children[1].innerHTML;
+    		output.appendChild(para);
+    	}
+    }
+}
---
+++ Submission/Day19/js/dom2.js
@@ -0,0 +1,16 @@
+// Solution with querySelector and querySelectorAll
+function getAllListItem() {
+    // 返回页面中所有li标签
+}
+
+function findAllHtmlSpanInOneSection(sectionId) {
+    // 返回某个section下所有span中内容为HTML的span标签
+}
+
+function findListItem(sectionId, spanCont) {
+    // 返回某个section下，所有所包含span内容为spanCont的LI标签
+}
+
+function getActiveLinkContent(sectionId) {
+    // 返回某个section下，class为active的链接中包含的文字内容
+}
